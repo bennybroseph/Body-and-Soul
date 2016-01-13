b6ebc7abc9ec784b12f5a207808e4315287a52e6
bennybroseph <bennybroseph@gmail.com>
2016-01-13 14:16:51 -0600

    Initial commit

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour
+{
+    // Use this for initialization
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    // Used to move actors
+    protected virtual void Move()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    bool IsActive;
+    [SerializeField]
+    float HitPoints;
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+
+    }
+
+    // Used to interact with 
+}
