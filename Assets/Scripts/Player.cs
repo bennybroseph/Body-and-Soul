6f10e113904d8c0cb74249eb893d0c96270ac1ac
bennybroseph <bennybroseph@gmail.com>
2016-01-19 11:44:17 -0600

    0.0.1
    End of Day commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,46 @@ public class Player : MonoBehaviour
     [SerializeField]
     float HitPoints;
 
+    [SerializeField]
+    private Rigidbody2D MyRigidbody;
+
+    bool ShouldJump = true;
+
+    protected virtual void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
     // Use this for initialization
     protected virtual void Start()
     {
-
+        Debug.Log("Start");
     }
 
-    // Update is called once per frame
-    protected virtual void Update()
+    protected virtual void FixedUpdate()
     {
+        float fMoveHorizontal = Input.GetAxis("Horizontal");
+        float fJump = 0;
 
+        //if (ShouldJump)
+        //{
+            if((fJump = Input.GetAxis("Jump")) > 0)
+            {
+              //  ShouldJump = false;
+            }
+       // }
+
+        Vector2 v2Movement = new Vector2(fMoveHorizontal * 1000 * Time.deltaTime, fJump * 8000 * Time.deltaTime);
+
+        Mathf.Clamp(v2Movement.x, 10, 100);
+        MyRigidbody.AddForce(v2Movement);
     }
+
+    //// Update is called once per frame
+    //protected virtual void Update()
+    //{
+        
+    //}
 
     // Used to interact with 
 }
