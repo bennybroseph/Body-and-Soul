2679851fdad608d6b71721d356e80703d2df6b99
Austin Morrell <austinlm19@gmail.com>
2016-01-25 17:12:25 -0600

    0.0.1.2.1

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+    public bool humanPlat;
+    public bool spiritPlat;
+    public float speed;
+    public float Spaces;
+
+    protected void MoveForward()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    protected void MoveBack()
+    {
+        transform.Translate(-Vector3.right * Time.deltaTime * speed);
+    }
+
+    protected void MoveUp()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    protected void MoveDown()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
+    }
+
+    protected void Snap()
+    {
+
+    }
+}
