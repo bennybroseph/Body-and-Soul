7086487b9db0d828374fd86e7d2a933dbeac08b0
Austin Morrell <austinlm19@gmail.com>
2016-01-26 14:08:43 -0600

    1.0.1.3.1
    Game Controller, Win Condition, My Scene

--- Assets/Scripts/Game_Controller.cs
+++ Assets/Scripts/Game_Controller.cs
@@ -1,17 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Game_Controller : MonoBehaviour {
+public class Game_Controller : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    private bool gameOver;
+    private bool restart;
+    private bool check;
+    private bool LevelComplete;
+    [SerializeField]
+    private GUIText gameOverText;
+    [SerializeField]
+    private GUIText restartText;
+    [SerializeField]
+    private GameObject ThePlayer;
+
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        LevelComplete = false;
+        gameOver = false;
+        restart = false;
+        check = true;
+        gameOverText.text = "";
+        restartText.text = "";
+    }
+
+    void Update()
     {
-	
-	}
+        if (check)
+        {
+            if (ThePlayer.transform.position.y <= -10)
+        {
+            GameOver();
+        }
+
+            if (gameOver)
+        {
+            restartText.text = "Press 'R' for Restart";
+            restart = true;
+            check = false;
+        }
+        }
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    public void YouWin()
+    {
+        gameOverText.text = "You Win!";
+        restart = true;
+        gameOver = true;
+    }
 }
