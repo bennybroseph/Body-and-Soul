3cacc5b9c01e62704c777dffbd5cc4af54029289
Austin Morrell <austinlm19@gmail.com>
2016-02-15 17:57:39 -0600

    1.0.9.1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,11 @@ public class Enemy : Actor {
 
     [SerializeField]
     protected GameObject Fire;
+    [SerializeField]
+    protected GameObject Player;
+    [SerializeField]
+    protected Vector3 ProjectileSpeed;
     protected Object clone1;
-    protected Object clone2;
-    protected Object clone3;
 
     protected override void Start ()
     {
@@ -18,8 +20,7 @@ public class Enemy : Actor {
 	protected override void Update ()
     {
         base.Update();
-        clone1 = Instantiate(Fire, new Vector3(), Quaternion.identity);
-        clone2 = Instantiate(Fire, new Vector3(), Quaternion.identity);
-        clone3 = Instantiate(Fire, new Vector3(), Quaternion.identity);
+        Transform clone1 = (Transform)Instantiate(Fire, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+        clone1.position += (Player.transform.position);
     }
 }
